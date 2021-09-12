caeb89330a615ed18bdf3a4788dc35f51e4ebcb7
Lav Sharma <lavsharma2016@gmail.com>
2021-09-12 22:34:23 +0530

    contact us and phone no. made clickable and working with gmail and dialing

--- lib/screens/contact_us/constants.dart
+++ lib/screens/contact_us/constants.dart
@@ -16,18 +16,61 @@ List<String> guestHouseTitles = [
 ];
 
 List<String> officeAddresses = [
-  ' Asha Kiran,\n 53, J.P. Road,\nAndheri (W)\n Mumbai-400058\n Email: ywcaandheri@rediffmail.com\n Ph: 022-26702831 / 022-26702872',
-  ' Anugraha Hostel,\n Sector 8 ,\n CBD-Belapur \n Navi Mumbai-400614\n Email: ywcabelapur@gmail.com\n Ph: 022-27570786',
-  ' 75 , Motlibai Street ,\n Near Maratha Mandir \n  Mumbai-400008\n Email: ybombaygs@gmail.com\n Ph: 022-23096544 / 022-23096555 / 022-23020469',
+  ' Asha Kiran,\n 53, J.P. Road,\nAndheri (W)\n Mumbai-400058\n',
+  ' Anugraha Hostel,\n Sector 8 ,\n CBD-Belapur \n Navi Mumbai-400614\n',
+  ' 75 , Motlibai Street ,\n Near Maratha Mandir \n  Mumbai-400008\n',
+];
+
+List<String> emailIdOffice = [
+  'ywcaandheri@rediffmail.com\n',
+  'ywcabelapur@gmail.com\n',
+  'ybombaygs@gmail.com\n'
+];
+
+List<String> contactNoOffice = [
+  '022-26702831',
+  '022-26702872',
+  '022-27570786',
+  '022-23096544',
+  '022-23096555',
+  '022-23020469'
 ];
 
 List<String> hostelAddresses = [
-  ' Asha Kiran,\n 53, J.P. Road,\nAndheri (W)\n Mumbai-400058\n Email: ywcaandheri@rediffmail.com\n Ph: 022-26702831 / 022-26702872',
-  ' Anugraha Hostel,\n Sector 8 ,\n CBD-Belapur \n Navi Mumbai-400614\n Email: ywcabelapur@gmail.com\n Ph: 022-27570786',
-  ' 75 , Motlibai Street ,\n Near Maratha Mandir \n  Mumbai-400008\n Email: ybombaygs@gmail.com\n Ph: 022-23096544 / 022-23096555 / 022-23020469',
+  ' Asha Kiran,\n 53, J.P. Road,\nAndheri (W)\n Mumbai-400058\n',
+  ' Anugraha Hostel,\n Sector 8 ,\n CBD-Belapur \n Navi Mumbai-400614\n',
+  ' 75 , Motlibai Street ,\n Near Maratha Mandir \n  Mumbai-400008\n',
+];
+
+List<String> emailIdHostel = [
+  'ywcaandheri@rediffmail.com\n',
+  'ywcabelapur@gmail.com\n',
+  'ybombaygs@gmail.com\n'
+];
+
+List<String> contactNoHostel = [
+  '022-26702831 ',
+  '022-26702872',
+  '022-27570786',
+  '022-23096544',
+  '022-23096555',
+  '022-23020469'
 ];
 
 List<String> guestHouseAddresses = [
-  ' Asha Kiran,\n 53, J.P. Road,\nAndheri (W)\n Mumbai-400058\n Email: ywcaandheri@rediffmail.com\n Ph:022-26702831 / 022-26702872',
-  ' 18 , Madame Cama Road ,\n Dr Ambedkar Statue Chowk Area,\n Fort, Mumbai - 4000018 \n Email: ymcaic@mtnl.net.in\n Ph: 022-22025053 / 022-66247222 / 022-2286814',
+  ' Asha Kiran,\n 53, J.P. Road,\nAndheri (W)\n Mumbai-400058\n',
+  ' 18 , Madame Cama Road ,\n Dr Ambedkar Statue Chowk Area,\n Fort, Mumbai - 4000018\n',
+];
+
+List<String> emailIdGuestHouse = [
+  'ywcaandheri@rediffmail.com\n',
+  'ywcabelapur@gmail.com\n'
+];
+
+List<String> contactNoGuestHouse = [
+  '022-26702831',
+  '022-26702872',
+  '022-22025053',
+  '022-66247222',
+  '022-2286814'
 ];
