689641bbafd263a6424a8349160316a84db6e5fd
Anish Nair <anishnair@Anishs-MacBook-Pro.local>
2021-09-28 09:54:33 +0530

    ios intial

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
@@ -8,6 +9,8 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    // Use Firebase library to configure APIs
+   
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
